dc4c5be36f1f473e2253f162e7c5ff25666aed48
Orest.Prustayko <Orest.Prustayko@eleks.com>
2014-12-18 10:56:46 +0200

    add & change files

--- __todo/Inheritance-classical-vs-object-create.js
+++ __todo/Inheritance-classical-vs-object-create.js
@@ -1,44 +1,55 @@
 var Person = {
-	constructor: function(name, age, gender) {
-		this.name = name;
-		this.age = age;
-		this.gender = gender || 'male';
-		return this;
-	},
-	sayHi: function() {
-		console.log(this.name + ' says Hi!');
-	}
-}, WebDeveloper = Object.create(Person);
+        constructor: function (name, age, gender) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender || 'male';
+            return this;
+        },
+        sayHi: function () {
+            console.log(this.name + ' says Hi!');
+        }
+    },
+    WebDeveloper = Object.create(Person);
 
-WebDeveloper.constructor = function(name, age, gender, skils) {
-	Person.constructor.apply(this, arguments);
-	this.skils = ['js','ruby','d3'] || skils;
+WebDeveloper.constructor = function (name, age, gender, skils) {
+    Person.constructor.apply(this, arguments);
+    this.skils = ['js', 'ruby', 'd3'] || skils;
 
     return this;
 };
 
-WebDeveloper.sayHi = function() {
-	console.log('I\'m working');
+WebDeveloper.sayHi = function () {
+    console.log('I\'m working');
 };
 
 var webDeveloper = Object.create(WebDeveloper).constructor('John', 22);
 
 //=====
-function Person (name, age, gender) {
-	this.name = name;
-	this.age = age;
-	this.gender = gender || 'male';
+function Person(options) {
+    this.name = options.name;
+    this.gender = options.gender || 'male';
 }
 
-Person.prototype.sayHi = function() {
-	console.log(this.name + ' says Hi!');
+Person.prototype.sayHi = function (greeting) {
+    return this.name + ', says Hi!';
 };
 
-var WebDeveloper = function(skils) {
-	Person.apply(this, arguments);
-	this.skils = ['js','ruby','d3'] || skils;
+function Developer(options) {
+    Person.apply(this, arguments);
+
+    this.skils = options.skils || ['OOP', 'TCP', 'HTTP'];
+}
+
+Developer.prototype = Object.create(Person.prototype);
+
+Developer.prototype.sayHi = function (greeting) {
+    return this.name + ', busy... I\'m codding!';
 };
 
-WebDeveloper.prototype = Object.create(Person.prototype);
+function WebDeveloper(options) {
+    Developer.apply(this, arguments);
 
-var webDeveloper = new WebDeveloper();
+    this.skils = this.skils.concat(options.skils || ['JavaScript', 'NodeJS', 'Angular']);
+}
+
+WebDeveloper.prototype = Object.create(Developer.prototype);
